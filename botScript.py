9cbfc02cecbf0689dcbe477398c272381964593e
denmun <denmun11@gmail.com>
2018-04-17 17:29:29 +0300

    Hello

---
+++ botScript.py
@@ -0,0 +1,54 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+getting = {}
+
+id_array = []
+
+updater = Updater(token = '583353411:AAE1qPbpf6SMq3xHP79C056u1EmJ3SYzSGY')
+dispatcher = updater.dispatcher
+
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text = 'Давай общаться?')
+
+def textMessage(bot, update):
+    if (update.message.chat_id not in id_array):
+        id_array.append(update.message.chat_id)
+        getting[update.message.chat_id] = []
+    print(getting) 
+    getting[update.message.chat_id].append(update.message.text)
+    if (getting[update.message.chat_id][len(getting[update.message.chat_id]) - 1] == "Triangle"):
+        bot.send_message(chat_id = update.message.chat_id, text = "Введите стороны треугольника")
+    elif (getting[update.message.chat_id][len(getting[update.message.chat_id]) - 2] == "Triangle"):
+        string = getting[update.message.chat_id][len(getting[update.message.chat_id]) - 1].split()
+        a = int(string[0])
+        b = int(string[1])
+        c = int(string[2])
+        if (a < b+c) and (b < c+a) and (c < b+a):
+            bot.send_message(chat_id = update.message.chat_id, text = "Можно")
+        else:
+            bot.send_message(chat_id = update.message.chat_id, text = "Нельзя")
+    elif (getting[update.message.chat_id][len(getting[update.message.chat_id]) - 1] == "реши"):
+        bot.send_message(chat_id = update.message.chat_id, text = "Введите a b c")
+    elif (getting[update.message.chat_id][len(getting[update.message.chat_id]) - 2] == "реши"):
+        string = getting[update.message.chat_id][len(getting[update.message.chat_id]) - 1].split()
+        a = int(string[0])
+        b = int(string[1])
+        c = int(string[2])
+        d = b*b - 4*a*c
+        x1 = (-b + d**(1/2))/(2*a)
+        x2 = (-b - d**(1/2))/(2*a)
+        res = "x1 = "+str(x1)+" x2 = "+str(x2)
+        bot.send_message(chat_id = update.message.chat_id, text = res)
+    else:
+        res = 'Получил ваше сообщение: '+ update.message.text
+        bot.send_message(chat_id = update.message.chat_id, text = res)
+    
+start_handler = CommandHandler('start', startCommand)
+text_handler = MessageHandler(Filters.text, textMessage)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(text_handler)
+
+updater.start_polling(clean=True)
+
+updater.idle()
